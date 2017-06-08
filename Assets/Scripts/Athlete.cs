ec90d6b6685e872d0f16701eb233311ac5b4efa5
Guillem <guillem.sannicolas@keradgames.com>
2017-06-08 17:43:03 +0200

    Create Athlete class

--- Assets/Scripts/Athlete.cs
+++ Assets/Scripts/Athlete.cs
@@ -1,19 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Athlete : MonoBehaviour 
 {
+	public string name, country, avatarUrl;
+	public string [] positions;
+	public int starRating, id;
+	public Texture2D playerImage, placeholderTexture;
+	public Text nameText, countryText, positionsText;
+	public RawImage profilePic;
+	public Image[] stars;
 
 	// Use this for initialization
-	void Start () 
+	public void Initialize(AthleteInfo athleteInfo)
 	{
-		
+		name = athleteInfo.name;
+		country = athleteInfo.country;
+		starRating = athleteInfo.star_rating;
+		positions = athleteInfo.positions;
+		avatarUrl = athleteInfo.avatar_url;
+		id = athleteInfo.id;
+		StartCoroutine (GetImage ());
 	}
-	
-	// Update is called once per frame
-	void Update () 
+
+	public void DisplayInfo () // triggered from GetImage()
 	{
-		
+		nameText.text = name;
+		//countryText.text = country;
+		SetStars ();
+		SetPositionsText ();
+		profilePic.texture = playerImage;
+	}
+
+	void SetStars()
+	{
+		for (int i = 0; i < starRating; i++) 
+		{
+			stars [i].color = Color.yellow;
+		}
+	}
+
+	void SetPositionsText()
+	{
+		for (int i = 0; i < positions.Length; i++) 
+		{
+			positionsText.text = string.Concat (positionsText.text, positions[i], "\n");
+		}
+	}
+
+	public IEnumerator GetImage()
+	{
+		WWW www = new WWW(avatarUrl);
+
+		// Wait for download to complete
+		yield return www;
+
+		// assign texture
+		if (www.texture != null) 
+		{
+			playerImage = www.texture;
+		}
+		else 
+		{
+			playerImage = placeholderTexture;
+		}
+		DisplayInfo ();
 	}
 }
