ad0ca4fa14dc4d58e9e263a7371e780384e89ed0
Guillem <guillem.sannicolas@keradgames.com>
2017-06-08 19:17:00 +0200

    Team average

---
+++ Assets/Scripts/CurrentTeam.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrentTeam : Singleton<CurrentTeam> {
+
+
+	public List<Athlete> athletes;
+
+	public void AddMember(Athlete athlete)
+	{
+		athletes.Add (athlete);
+	}
+
+	public void RemoveMember(Athlete athlete)
+	{
+		athletes.Remove (athlete);
+	}
+
+	public void ResetTeam()
+	{
+		for (int i = 0; i < athletes.Count; i++) 
+		{
+			athletes [i].transform.SetParent (ParentLayer.Instance.allAthletesContainer);
+			RemoveMember (athletes [i]);
+		}
+	}
+}
---
+++ Assets/Scripts/TeamAverage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TeamAverage : Singleton<TeamAverage> {
+
+	public Image[] stars;
+	public void Average()
+	{
+		ResetStars();
+		int starAmount = 0;
+		for (int i = 0; i < CurrentTeam.Instance.athletes.Count; i++) {
+			starAmount += CurrentTeam.Instance.athletes [i].starRating;
+		}
+		if(starAmount > 0)starAmount = Mathf.CeilToInt (starAmount / CurrentTeam.Instance.athletes.Count);
+		for (int i = 0; i < starAmount; i++) 
+		{
+			stars [i].color = Color.yellow;
+		}
+	}
+
+	public void ResetStars()
+	{
+		for (int i = 0; i < stars.Length; i++) 
+		{
+			stars [i].color = Color.white;
+		}
+	}
+
+}
