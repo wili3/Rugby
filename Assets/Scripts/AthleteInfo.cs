b9058ff7ca80ee398aba90ad9310ff088277e2f8
Guillem <guillem.sannicolas@keradgames.com>
2017-06-08 14:12:23 +0200

    Parse athletes info

---
+++ Assets/Scripts/AthleteInfo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+
+[Serializable]
+public class AthleteInfo 
+{
+	public string name;
+	public string country;
+	public int star_rating;
+	public string[] positions;
+	public bool is_injured;
+	public int id;
+	public string avatar_url;
+	public DateTime last_played;
+}
---
+++ Assets/Scripts/AthleteParser.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+
+public class AthleteParser : Singleton<AthleteParser> 
+{
+	[Serializable]
+	public class AthleteInfosCollection
+	{
+		public AthleteInfo[] athletes;
+	}
+
+	public AthleteInfosCollection athletesInfo;
+
+	void Start()
+	{
+		string athletesJson = Resources.Load<TextAsset> ("athletes").text;
+
+		if (athletesJson != string.Empty) 
+		{
+			athletesInfo = JsonUtility.FromJson<AthleteInfosCollection> (athletesJson);
+			Debug.Log(athletesInfo.athletes.Length);
+		}
+	}
+}
